3c5c831d1a762db2b0e2df2047e01f721e55a02d
ronaldoaf <ronaldo.a.f@gmail.com>
2018-03-01 10:48:08 -0300

    g2 e shortener

--- cubo.py
+++ cubo.py
@@ -75,94 +75,224 @@ class Cubo:
             (11,(1,3,4,6)),
             (12,(1,3,4,6)),
             (14,(1,3,4,6)),
+            
             (17,(1,3,4,6)),
             (19,(9,11,12,14,17)),
             (20,(9,11,12,14,17)),
             (22,(1,3,4,6,19,20)),
+            
             (25,(9,11,12,14,22)),
             (27,(9,11,12,14,22)),
             (28,(9,11,12,14,22)),
-            (30,(9,11,12,14,22)),
+            (30,(9,11,12,14,22)),            
+            
             (33,(1,3,4,6,19,20,25,27,28,30)),
             (35,(1,3,4,6,19,20,25,27,28,30)),
             (36,(1,3,4,6,19,20,25,27,28,30)),
             (38,(1,3,4,6,19,20,25,27,28,30)),
+            
             (41,(1,3,4,6,19,20,25,27,28,30)),
             (43,(9,11,12,14,22,33,35,36,38,41)),
             (44,(9,11,12,14,22,33,35,36,38,41)),
-            (46,(1,3,4,6,19,20,25,37,28,43,44))
+            (46,(1,3,4,6,19,20,25,37,28,43,44))            
             ]
         for r in res:
             for pos in r[1]:
                 if self.config[pos]==r[0]:
-                    #print [r[0],pos]
+                    print [r[0],pos]
                     self.notIsG1=[r[0],pos]
                     return False
         return True
 
+    
+
+    def isG2(self):
+        res=[
+            (9,(1,3,4,6)),
+            (11,(1,3,4,6,9)),
+            (12,(1,3,4,6,9)),
+            
+            (14,(1,3,4,6,11,12)),            
+            (17,(1,3,4,6,11,12)),
+            (19,(1,3,4,6,11,12)),
+            (20,(1,3,4,6,11,12)),
+            (22,(1,3,4,6,11,12)),
+            
+            (25,(9,11,12,14,17,19,20,22)),
+            (27,(9,11,12,14,17,19,20,22)),
+            (28,(9,11,12,14,17,19,20,22)),
+            (30,(9,11,12,14,17,19,20,22)),           
+            
+            (33,(1,3,4,6,11,12,25,27,28,30)),
+            
+            
+            (36,(1,3,4,6,9,14,17,19,20,25,27,28,30,33)),
+            
+            (38,(1,3,4,6,11,12,25,27,28,30,35,36)),
+            
+            (41,(1,3,4,6,11,12,25,27,28,30,35,36)),
+            (43,(1,3,4,6,11,12,25,27,28,30,35,36)),
+            (44,(1,3,4,6,11,12,25,27,28,30,35,36)),
+            (46,(1,3,4,6,11,12,25,27,28,30,35,36)),        
+            ]
+        for r in res:
+            for pos in r[1]:
+                if self.config[pos]==r[0]:
+                    print [r[0],pos]
+                    self.notIsG2=[r[0],pos]
+                    return False
+        return True
+
+
+def shortener(move_list):    
+    def short(move_list):        
+        if len(move_list)<=1: return move_list
+        if move_list[0][0]==move_list[1][0]:
+            move_list[1]=move_list[1][0]+ str((int(move_list[0][1])+int(move_list[1][1]))%4)
+            move_list=move_list[2:] if move_list[1][1]=='0' else move_list[1:]
+        else:
+            move_list=[move_list[0]]+short(move_list[1:])
+        return  move_list
+
+    string=False
+    if type(move_list)==str:
+        move_list=move_list.split(' ')
+        string=True
+    shorted_move_list=short(move_list)
+    while shorted_move_list!=move_list:
+        
+        move_list=shorted_move_list
+        shorted_move_list=short(move_list)
+    return ' '.join(move_list) if string else move_list
 
 
 print "Novo Cubo todo arrumadinho"
 cubo=Cubo()
 
-
 cubo.shuffle()
-
-
-dictG1={1:('D','L'),
-        3:('F','L'),
-        4:('B','L'),
-        6:('U','L'),
-        9:('L','F'),
-        11:('D','F'),
-        12:('U','F'),
-        14:('R','F'),
-        17:('L','U'),
-        19:('F','U'),
-        20:('B','U'),
-        22:('R','U'),
-        25:('U','R'),
-        27:('F','R'),
-        28:('B','R'),
-        30:('D','R'),
-        33:('L','B'),
-        35:('U','B'),
-        36:('D','B'),
-        38:('R','B'),
-        41:('L','D'),
-        43:('B','D'),
-        44:('F','D')
+#print cubo.config
+#cubo.config=[26, 41, 24, 25, 4, 2, 14, 7, 23, 22, 34, 38, 11, 31, 17, 10, 40, 27, 18, 44, 9, 5, 36, 42, 16, 43, 8, 6, 46, 39, 35, 29, 32, 33, 21, 3, 19, 0, 30, 47, 15, 1, 37, 12, 28, 13, 20, 45]
+dictG1={1:('D1','L1'),
+        3:('F1','L1'),
+        4:('B1','L1'),
+        6:('U1','L1'),
+        9:('L1','F1'),
+        11:('D1','F1'),
+        12:('U1','F1'),
+        14:('R1','F1'),
+        17:('L1','U1'),
+        19:('F1','U1'),
+        20:('B1','U1'),
+        22:('R1','U1'),
+        25:('U1','R1'),
+        27:('F1','R1'),
+        28:('B1','R1'),
+        30:('D1','R1'),
+        33:('L1','B1'),
+        35:('U1','B1'),
+        36:('D1','B1'),
+        38:('R1','B1'),
+        41:('L1','D1'),
+        43:('B1','D1'),
+        44:('F1','D1')
         }
+
+dictG2={1:('D2','L1'),
+        3:('F1','L1'),
+        4:('B1','L1'),
+        6:('U2','L1'),
+        9:('L1','F1'),
+        11:('D2','F1'),
+        12:('U2','F1'),
+        14:('R1','F1'),
+        17:('L1','U2'),
+        19:('F1','U2'),
+        20:('B1','U2'),
+        22:('R1','U2'),
+        25:('U2','R1'),
+        27:('F1','R1'),
+        28:('B1','R1'),
+        30:('D2','R1'),
+        33:('L1','B1'),
+        35:('U2','B1'),
+        36:('D2','B1'),
+        38:('R1','B1'),
+        41:('L1','D2'),
+        43:('B1','D2'),
+        44:('F1','D2')
+        }
+
+c=0
+repeat=0
+last_move=''
+saida=[]
+
+while not cubo.isG1() and c<1000: 
+    c+=1
+    
+    next_move=dictG1[cubo.notIsG1[1]][0]
+    if next_move==last_move:
+        repeat+=1
+    else:
+        repeat=0
+
+    if repeat==3:
+        next_move=dictG1[cubo.notIsG1[1]][1]
+    
+    print cubo.notIsG1, next_move, c
+    saida+=[next_move]
+    cubo.permute(next_move)
+    
+    last_move=next_move
+
+print shortener(saida)
+#print saida
+
+
+
+
+
+
 
 
 
 c=0
 repeat=0
 last_move=''
-while not cubo.isG1() and c<1000: 
+saida=[]
+
+while not cubo.isG2() and c<1000: 
     c+=1
     
-    next_move=dictG1[cubo.notIsG1[1]][0]+'1'
+    next_move=dictG2[cubo.notIsG2[1]][0]
     if next_move==last_move:
         repeat+=1
     else:
         repeat=0
 
-    if repeat==3: next_move=dictG1[cubo.notIsG1[1]][1]+'1'
-
-    print cubo.notIsG1, next_move, c
+    if repeat==3:
+        next_move=dictG2[cubo.notIsG2[1]][1]
+    
+    print cubo.notIsG2, next_move, c
+    saida+=[next_move]
     cubo.permute(next_move)
     
     last_move=next_move
-    
+
+
+print shortener(saida)
+#print saida
 '''
+
+
+
 for j in range(1000):
     for i in range(20):
-        cubo.permute(['L1','R1','F1','B1','U2','D2'][randrange(6)])
+        cubo.permute(['L2','R2','F2','B2','U2','D2'][randrange(6)])
 
-    print cubo.isG1()
+    #print cubo.isG2()
 
-   #print cubo.config
+    print cubo.config
 
 #print cubo.isG1()
 #print cubo.config
@@ -178,5 +308,4 @@ for j in range(1000):
 
 
 #print "Cubo embaralhado"
-
 '''
